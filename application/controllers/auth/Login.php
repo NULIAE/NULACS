e0bc4e45c3b9eff315c4cc6e953ad9cbcedb0a1b
Binu Nair <binu.nair@thoughtfocus.com>
2021-02-09 13:07:27 +0530

    Auth Changes

--- application/controllers/auth/Login.php
+++ application/controllers/auth/Login.php
@@ -54,46 +54,66 @@ class Login extends CI_Controller
 	 */
 	public function authenticate()
 	{
-
-		//XSS Filter all the input post fields
-		$data = $this->input->post(NULL, TRUE);
-
-		//Check whether user with the email id exists on `users` table
-		$user_data = $this->User_model->check_user($data['email']);
-
-		if ( ! empty($user_data) && $user_data['user_status'] === "1" )
+		if(isset($_POST))
 		{
-			//User exists. Check for the valid password
-			if ($data['password'] == $user_data['user_password']) 
+			//XSS Filter all the input post fields
+			$data = $this->input->post(NULL, TRUE);
+
+			if(isset($data['email']) && isset($data['password']))
 			{
-				//Update the last login time in `users` table
-				$this->User_model->update_last_login($user_data['user_id']);
+				//Check whether user with the email id exists on `users` table
+				$user_data = $this->User_model->check_user($data['email']);
 
-				$log_data = array(
-					'user_id' => $user_data['user_id'],
-					'affiliate_id' => $user_data['affiliate_id'],
-					'note' => 'Logged in'
-				);
+				if ( ! empty($user_data) && $user_data['user_status'] === "1" )
+				{
+					//User exists. Check for the valid password
+					if ($data['password'] == $user_data['user_password']) 
+					{
+						//Update the last login time in `users` table
+						$this->User_model->update_last_login($user_data['user_id']);
 
-				//Log user activities
-				$this->User_model->user_log($log_data);
+						$log_data = array(
+							'user_id' => $user_data['user_id'],
+							'affiliate_id' => $user_data['affiliate_id'],
+							'note' => 'Logged in'
+						);
 
-				unset($user_data['user_password']);
-				
-				$user_data['email'] = $data['email'];
-				$user_data['logged_in'] = TRUE;
-				
-				$this->session->set_userdata($user_data);
+						//Log user activities
+						$this->User_model->user_log($log_data);
 
-				
-				redirect(base_url('/'));
-			} 
-		} 
+						unset($user_data['user_password']);
+						
+						$user_data['email'] = $data['email'];
+						$user_data['logged_in'] = TRUE;
+						
+						$this->session->set_userdata($user_data);
 
-		//User with email id doesn't exists or invalid password. Back to login
-		$this->session->set_flashdata('error', 'Invalid username or password.');
-
-		redirect(base_url('/login'));
+						
+						redirect(base_url('/'));
+					} 
+				}
+				else
+				{
+					//User with email id doesn't exists or invalid password. Back to login
+					$this->session->set_flashdata('error', 'Invalid username or password.');
+			
+					redirect(base_url('/login'));
+				}
+			}
+			else
+			{
+				//User with email and password required. Back to login
+				$this->session->set_flashdata('error', 'Username and Password are required.');
+		
+				redirect(base_url('/login'));
+			}
+		}
+		else
+		{
+			$this->session->set_flashdata('error', 'Invalid username or password.');
+		
+			redirect(base_url('/login'));
+		}
 	}
 	
 	/**
@@ -103,15 +123,17 @@ class Login extends CI_Controller
 	 */
 	public function logout()
 	{
-
-		$log_data = array(
-			'user_id' => $this->session->user_id,
-			'affiliate_id' => $this->session->affiliate_id,
-			'note' => 'Logged out'
-		);
-
-		//Log user activities
-		$this->User_model->user_log($log_data);
+		if(isset($this->session->user_id) && isset($this->session->affiliate_id))
+		{
+			$log_data = array(
+				'user_id' => $this->session->user_id,
+				'affiliate_id' => $this->session->affiliate_id,
+				'note' => 'Logged out'
+			);
+	
+			//Log user activities
+			$this->User_model->user_log($log_data);
+		}
 		
 		$this->session->sess_destroy();
 
