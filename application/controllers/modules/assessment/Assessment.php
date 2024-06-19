6d501ba868c5aa8160ce1db1cae2532106f3fb7c
sandeep <sandeep.sk@infospica.com>
2024-06-19 18:36:54 +0530

    code added

--- application/controllers/modules/assessment/Assessment.php
+++ application/controllers/modules/assessment/Assessment.php
@@ -1196,7 +1196,18 @@ class Assessment extends MY_Controller
   public function censusPdfExport() {
 	$data = $this->input->get();
 	$dompdf = new Dompdf();
-	$report_year = isset($data['year']) ? $data['year'] : NULL;
+	$report_year = $data['year'] ? $data['year'] : NULL;
+	$status = $data['status'] ? $data['status'] : NULL;
+	$affiliate_id = $data['affiliate'] ? $data['affiliate'] : NULL;
+	$report_details = $this->Affiliate_model->report_details($report_id);
+
+	$report_data = $this->Affiliate_model->census_report($report_year, $affiliate_id, $status);
+	$arrContextOptions=array(
+		"ssl"=>array(
+			"verify_peer"=>false,
+			"verify_peer_name"=>false,
+		),
+	); 
 	$html = '<!DOCTYPE html>
 			 <html lang="en">
 			 <head>
