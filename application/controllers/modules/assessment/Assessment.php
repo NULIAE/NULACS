2486a2485bed550134ec2516e5212e884e942458
sandeep <sandeep.sk@infospica.com>
2024-08-02 11:28:32 +0530

    image code commented

--- application/controllers/modules/assessment/Assessment.php
+++ application/controllers/modules/assessment/Assessment.php
@@ -2212,15 +2212,15 @@ class Assessment extends MY_Controller
      //$image_path = str_replace('./uploads/Documents/','',$report_data[0]['field_photo_title']);
 	  $field_photo_title = $report_data[0]['field_photo_title']; 
       //print_r($field_photo_title);die;
-      if($report_data[0]['field_photo_title'] != "" || $report_data[0]['field_photo_title']){
-		if (file_exists('/var/www/html/nul_adms/resources/images/profile/'.$field_photo_title)) {
-			$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/profile/'.$field_photo_title, 'width' => 500, 'height' => 97,'media-type' => 'image/jpg'));
-	  	} else {
-			$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/demo.jpg', 'width' => 100, 'height' => 85,'media-type' => 'image/jpg'));
-		}
-	  }else{
-		$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/demo.jpg', 'width' => 100, 'height' => 85,'media-type' => 'image/jpg'));	
-	  }
+    //   if($report_data[0]['field_photo_title'] != "" || $report_data[0]['field_photo_title']){
+	// 	if (file_exists('/var/www/html/nul_adms/resources/images/profile/'.$field_photo_title)) {
+	// 		$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/profile/'.$field_photo_title, 'width' => 500, 'height' => 97,'media-type' => 'image/jpg'));
+	//   	} else {
+	// 		$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/demo.jpg', 'width' => 100, 'height' => 85,'media-type' => 'image/jpg'));
+	// 	}
+	//   }else{
+	// 	$templateProcessor->setImageValue('IMAGE', array('path' => '/var/www/html/nul_adms/resources/images/demo.jpg', 'width' => 100, 'height' => 85,'media-type' => 'image/jpg'));	
+	//   }
     //   if($report_data[0]['field_photo_title'] == ""){
     //     $templateProcessor->setValue('IMAGE', "");
 	//   	//$templateProcessor->setImageValue('IMAGE', array('path' => $field_photo_title, 'media-type' => 'image/jpg'));
